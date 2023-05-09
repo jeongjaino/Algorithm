4fcb29f8ad71c0771463ede6c3e9732d66241b54
JinHo Jeong <msb3302@naver.com>
2023-05-09 19:22:49 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 11.25 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -1,14 +1,7 @@
 class Solution {
     fun solution(strings: Array<String>, n: Int): Array<String> {
-        val list : Array<Pair<Char, String>> = Array(strings.size){ Pair('a', "") }
-        for(i in strings.indices){
-            list[i] = Pair(strings[i][n], strings[i]) 
-        }
-        list.sortWith(compareBy<Pair<Char, String>>{ it.first }.thenBy { it.second })
-        val answer = Array<String>(strings.size){""}
-        for(i in list.indices){
-            answer[i] = list[i].second
-        }
+        val answer = strings
+        answer.sortWith(compareBy<String>{ it[n] }.thenBy{ it })
         return answer
     }
 }
