c13f7cd64152348e631bec8657f29d169cbc6f4e
JinHo Jeong <msb3302@naver.com>
2023-05-09 14:55:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int, m: Int): IntArray {
+        val gcd = gcd(n, m)
+        return intArrayOf(gcd, n * m / gcd)
+    }
+    
+    fun gcd(a : Int, b: Int): Int{
+        if(b == 0){
+            return a
+        }else{
+            return gcd(b, a % b)
+        }
+    }
+}
