051cc0d9b05c3e100e85e5ba6ba7349036cd49b7
JinHo Jeong <msb3302@naver.com>
2023-05-10 15:36:49 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 19.33 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(food: IntArray): String {
+        var answer: String = ""
+        for(i in 1 until food.size){
+            answer += (i.toString()).repeat(food[i] / 2)
+        }
+        return answer + "0" + answer.reversed()
+    }
+}
