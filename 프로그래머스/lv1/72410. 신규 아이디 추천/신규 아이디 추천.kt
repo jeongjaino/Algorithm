a78d0f56e0bf69e24ccabba57cfe0d1b35fab266
JinHo Jeong <msb3302@naver.com>
2023-05-11 14:43:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 25.03 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.kt
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.kt
@@ -9,47 +9,21 @@ class Solution {
         }
         answer = new
 
-        answer = removeDuplicate(answer)
+        answer = answer.replace("[.]*[.]".toRegex(), ".")
 
-        answer = removeDot(answer)
+        answer = answer.removePrefix(".").removeSuffix(".")
         if(answer.isEmpty()){
             answer = "a"
         }
         
         if(answer.length >= 16){
-            answer = answer.slice(0 until 15)
-            answer = removeDot(answer)
+            answer = answer.slice(0 until 15).removeSuffix(".")
         }
         
         while(answer.length < 3){
            answer += answer.last()
         }
         
-        return answer
-    }
-    
-    fun removeDuplicate(s: String): String{
-        var answer = ""
-        for(i in 0 until s.length - 1){
-            if(s[i] == '.'){
-                if(s[i + 1] == '.'){
-                    continue
-                }
-            }
-            answer += s[i]
-        }
-        return answer + s.lastOrNull()
-    }
-
-    
-    fun removeDot(s: String): String{
-        var answer = s
-        if(answer.lastOrNull() == '.'){
-            answer = answer.slice(0 until answer.lastIndex)
-        }
-        if(answer.firstOrNull() == '.'){
-            answer = answer.slice(1 .. answer.lastIndex)
-        }
         return answer
     }
 }
