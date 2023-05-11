2a0c41a34cd32c279b03fe2ba33dd54ee85167b9
JinHo Jeong <msb3302@naver.com>
2023-05-12 01:46:41 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.kt
+++ 프로그래머스/lv2/42842. 카펫/카펫.kt
@@ -1,8 +1,5 @@
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
-        // 총 타일의 갯수로 모든 배열 구성하기 col < row 
-        // 테두리의 경우 상하좌우 중 하나가 벗어난 경우 
-        // 카운트하는 함수. 
         val tiles = brown + yellow
         var col = 1
         var row = tiles / col
