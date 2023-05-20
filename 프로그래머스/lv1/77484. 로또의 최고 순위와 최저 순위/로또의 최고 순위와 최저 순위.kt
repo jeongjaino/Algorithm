403228584482101482a106ea4af9a26ab5cb6080
JinHo Jeong <msb3302@naver.com>
2023-05-20 13:16:37 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 13.74 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        // 일치하는 갯수 + 0의 갯수 = 최대? 
-        // 그냥 일치하는 갯수가 최소 
         var count = 0
         for(i in win_nums){
             if(lottos.contains(i)){
@@ -9,10 +7,7 @@ class Solution {
             }
         }
         val zeroCount = lottos.count{ it == 0 }
-        var maxResult = 6
-        if(count + zeroCount > 1){
-            maxResult -= (count + zeroCount - 1)
-        }
-        return intArrayOf(maxResult, if(count > 1) 6 - (count - 1) else 6)
+        return intArrayOf(rank(count + zeroCount), rank(count))
     }
+    fun rank(count : Int): Int = if(count > 1) (7 - count) else 6
 }
