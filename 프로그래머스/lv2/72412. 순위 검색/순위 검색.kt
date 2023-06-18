f676c7bf91ba5fdd2d4514a29c7ae6cb289f9962
JinHo Jeong <msb3302@naver.com>
2023-06-18 19:42:07 +0900

    [level 2] Title: 순위 검색, Time: 1123.51 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.kt
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.kt
@@ -1,29 +1,32 @@
 class Solution {
-    private var infoMap = hashMapOf<String, ArrayList<Int>>()
+    private var infoMap = hashMapOf<String, MutableList<Int>>()
     fun solution(info: Array<String>, query: Array<String>): IntArray {
+        val queryList : List<List<String>> = query.map{
+            it.replace("and ", "").split(" ")
+        }
         info.forEach{
             createInfoKey(it.split(" "), "", 0)
         }
         for(key in infoMap.keys){
             infoMap[key]!!.sort()
         }
-        val result = IntArray(query.size){ 0 }
-        val queryList : List<List<String>> = query.map{
-            it.replace("and ", "").split(" ")
-        }
-        query.forEachIndexed{ index, it ->
-            val q = it.replace(" and ", "").split(" ")
-            if(infoMap.containsKey(q[0])){
-                result[index] = (binarySearch(infoMap[q[0]]!!, q[1].toInt()))
+        val result = mutableListOf<Int>()
+        queryList.forEach{ it -> 
+            val key = it.slice(0 until it.size - 1).joinToString("")
+            val score = it.last().toInt()
+            if(infoMap.containsKey(key)){
+                result.add(binarySearch(infoMap[key]!!.toList(), score))
+            }else{
+                result.add(0)
             }
         }
-        return result
+        return result.toIntArray()
     }
 
     fun createInfoKey(info: List<String>, key: String, count: Int){
         if(count == 4){
             if(!infoMap.containsKey(key)){
-                infoMap.put(key, ArrayList<Int>())
+                infoMap.put(key, mutableListOf())
             }
             infoMap[key]!!.add(info[4].toInt())
             return
@@ -31,9 +34,9 @@ class Solution {
         createInfoKey(info, key + "-", count + 1)
         createInfoKey(info, key + info[count], count + 1)
     }
-
+    
     fun binarySearch(list : List<Int>, score: Int): Int{
-        var start = 0
+        var start = 0 
         var end = list.size - 1
         while(start <= end){
             val mid = (start + end) / 2
