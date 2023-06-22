87692d960443c4b151455a56b28f5b7ab215d11e
JinHo Jeong <msb3302@naver.com>
2023-06-22 16:29:30 +0900

    [unrated] Title: 과제 진행하기, Time: 39.42 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.kt
@@ -0,0 +1,60 @@
+class Solution {
+    fun solution(plans: Array<Array<String>>): Array<String> {
+        plans.sortWith(compareBy { it[1] })
+        val result = mutableListOf<String>()
+        val leftWork = ArrayDeque<Pair<String, Int>>()
+        var currentWork = 0
+        while(result.size < plans.size){
+            val time = if(currentWork < plans.lastIndex) { // 다음 작업간의 시간 차이 
+                getTime(plans[currentWork + 1][1], plans[currentWork][1])
+            }
+            else if(currentWork == plans.lastIndex){
+                plans[currentWork][2].toInt() // 마지막 작업인 경우 
+            }
+            else{
+                break
+            }
+            // 다음 작업간의 시간차이 - 과제시간
+            var difference = time - plans[currentWork][2].toInt()
+            if(difference >= 0){ // 시간차이 > 과제 시간 
+                result.add(plans[currentWork][0])
+                while(difference > 0 && leftWork.isNotEmpty()){
+                    val work = leftWork.last()
+                    leftWork.removeLast()
+                    difference -= work.second
+                    if(difference >= 0){
+                        // 남은 시간의 남은 작업 시간이 차이보다 작은 경우 
+                        result.add(work.first)
+                    }
+                    else{
+                        leftWork.add(Pair(work.first, -difference))
+                    }
+                }
+            }
+            else{ // 시간차이 < 과제 시간 
+                leftWork.add(Pair(plans[currentWork][0], -difference))
+            }
+            currentWork += 1
+        }
+        for(i in leftWork.lastIndex downTo 0){
+            result.add(leftWork[i].first)
+        }
+        return result.toTypedArray()
+    }
+
+    // aTime > bTime
+    fun getTime(aTime : String, bTime : String): Int{
+        val ah = aTime.slice(0 until 2).toInt()
+        val bh = bTime.slice(0 until 2).toInt()
+        val am = aTime.slice(3 until 5).toInt()
+        val bm = bTime.slice(3 until 5).toInt()
+        var result = (ah - bh) * 60
+        if(am - bm > 0){
+            result += (am - bm)
+        }
+        else{
+            result -= (bm - am)
+        }
+        return result
+    }
+}
